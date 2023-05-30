108d95e9ce0981cc97b11950b5d86a3146f6e388
JHerrera458 <julianherrera33@gmail.com>
2023-05-30 17:27:21 -0500

    Bcrypt y auth controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,5 +1,25 @@
 import { Request, Response } from "express";
+import { JwtService } from "../business/services/jwt.service";
+import { MongoService } from "../business/services/mongo.service";
+import { ResponseModel } from "../business/models/response.model";
 
-export const login = (req: Request, res: Response) => {
-    return res.send("login")
+const jwtService = new JwtService()
+const collection = "users"
+
+export const login = async (req: Request, res: Response) => {
+    const { email, password } = req.body
+    console.log(email);
+    console.log(password);  
+    const responseModel = new ResponseModel(true, "Consulta exitosa", [])
+    try {
+        const mongoService = new MongoService()
+        const user = await mongoService.getUserByCredentials(collection, email, password)
+        responseModel.info = user
+        return res.send(responseModel)
+    } catch (error) {
+        console.log(error);
+        responseModel.ok = false
+        responseModel.message = "Error al consultar usuarios"
+        return res.send(responseModel)
+    }
 }
--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from "express";
 import { ResponseModel } from "../business/models/response.model";
 import { MongoService } from "../business/services/mongo.service";
+import { BCrypt } from "../business/services/bcrypt.service";
 
 const collection = "users"
+const bcrypt = new BCrypt()
 
 export const getUsers = async (req: Request, res: Response) => {
     const responseModel = new ResponseModel(true, "Consulta exitosa", [])
@@ -27,6 +29,12 @@ export const createUser = async (req: Request, res: Response) => {
     const responseModel = new ResponseModel(true, "Creación exitosa", [])
     try {
         const user = req.body
+        const passHash = bcrypt.createHash(user.password)
+        user.password = passHash
+
+        const userInfo = {...user}
+        delete userInfo.password
+
         const service = new MongoService()
         if (user) {
             const response = await service.insertItem(collection, user)
@@ -40,8 +48,8 @@ export const createUser = async (req: Request, res: Response) => {
     } catch (error) {
         console.log(error);
         responseModel.ok = false
-        responseModel.message = "Error al crear usuario"
-        return res.send(responseModel)
+        responseModel.message = "Faltan campos obligatorios"
+        return res.status(400).send(responseModel)
     }
 }
 
